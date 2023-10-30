55341de9c1d44b65846304303241021e160dc82f
Zrazhevskii <vbn-134@yandex.ru>
2023-10-30 15:40:09 +0300

    написание компонента

--- src/components/Сonvector.tsx
+++ src/components/Сonvector.tsx
@@ -1,35 +1,80 @@
 import React, { useState } from 'react';
-import { IColor } from './models';
 
 interface IItem {
-    hex: string,
-    rgb: string,
+    rgb: string | undefined;
 }
 
-const Сonvectors = () => {
-    const [color, setColor] = useState<IItem>({
-        hex: '',
-        rgb: '',
-    })
-    const {hex, rgb} = color;
+const checkHex = (value: string) => {
+    const regex = /^#([0-9a-f]{6}|[0-9a-f]{3})$/i;
+    const arr = [];
 
-    const hendler = (e: any) => {
+    if (regex.test(value) ? true : false) {
+        arr.push(parseInt(value.slice(1, 3), 16));
+        arr.push(parseInt(value.slice(3, 5), 16));
+        arr.push(parseInt(value.slice(5, 7), 16));
+        return `rgb(${arr.toString()})`;
+    } else {
+        return 'Ошибка!'
+    }
+    
+};
+
+const Сonvectors = () => {
+    const [input, setInput] = useState<IItem>({
+        rgb: '',
+    });
+
+    let { rgb } = input;
+
+    const handler = (e: any) => {
         e.preventDefault();
 
-        console.log(e.target.value)
-    } 
+        let value = e.target.value;
+
+        if (value.length < 7 || value.length > 7) {
+            setInput({
+                rgb: 'Ошибка!',
+            });
+        } else if (value.length === 7) {
+            let checkValue: string | undefined = checkHex(value);
+            setInput({
+                rgb: checkValue,
+            });
+        }
+    };
 
     return (
-        <div className='flex justify-center mx-auto h-screen bg-white mt-40 py-2.5'>
-            <form action="" className='flex flex-col items-center w-44 h-40'>
-                <label htmlFor="check" className=''>Цвет</label>
-                <input type="text" id='check' onInput={(e) => hendler(e)} className='input mx-auto border border-gray-300 text-sm rounded-lg block w-40 p-2.5' required/>
-                <label htmlFor="result" className=''>RBG</label>
-                <input type="text" id='result' className='result mx-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-40 p-2.5' required/>
+        <div
+            className='flex justify-center mx-auto h-screen pt-40 py-2.5'
+            style={{ backgroundColor: `${rgb}` }}
+        >
+            <form
+                action=''
+                className='flex flex-col bg-gray-200 border border-gray-800 items-center w-44 h-40'
+            >
+                <label htmlFor='check' className=''>
+                    HEX:
+                </label>
+                <input
+                    type='text'
+                    id='check'
+                    onInput={(e) => handler(e)}
+                    className='input mx-auto text-center border border-gray-300 text-sm rounded-lg block w-40 p-2.5'
+                    required
+                />
+                <label htmlFor='result' className=''>
+                    RGB:
+                </label>
+                <input
+                    type='text'
+                    id='result'
+                    value={rgb}
+                    className='result mx-auto bg-gray-50 border border-gray-300 text-center text-gray-900 text-sm rounded-lg block w-40 p-2.5'
+                    disabled
+                />
             </form>
         </div>
-    )
-    
+    );
 };
 
 export default Сonvectors;
